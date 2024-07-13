1653c6c754de3ef0b105ea343fbdd79bb8161c4c
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2024-07-13 15:12:36 +0530

    baseline bot config

--- tradingbot.py
+++ tradingbot.py
@@ -2,8 +2,8 @@ from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
-from datetime import datetime 
-from alpaca_trade_api import REST 
+from datetime import datetime
+from alpaca_trade_api import REST
 from datetime import timedelta
 
 import os
@@ -15,23 +15,28 @@ API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 BASE_URL = os.getenv("BASE_URL")
 
-ALPACA_CREDS = {
-    "API_KEY": API_KEY,
-    "API_SECRET": API_SECRET,
-    "PAPER": True
-}
+ALPACA_CREDS = {"API_KEY": API_KEY, "API_SECRET": API_SECRET, "PAPER": True}
+
 
 class MLTrader(Strategy):
-    def initialize(self):
-        pass
+    def initialize(self, symbol: str = "SPY"):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+
     def on_trading_iteration(self):
-        pass
+        if self.last_trade == None:
+            order = self.create_order(self.symbol, 10, "buy", type="market")
+            self.submit_order(order)
+            self.last_trade = "buy"
+
 
 start_date = datetime(2024, 6, 1)
 end_date = datetime(2024, 6, 30)
 
-
 broker = Alpaca(ALPACA_CREDS)
-strategy = MLTrader(name="mlstrat", broker=broker, parameters={})
+strategy = MLTrader(name="mlstrat", broker=broker, parameters={"symbol": "SPY"})
 
-strategy.backtest(YahooDataBacktesting, start_date, end_date, parameters={})
+strategy.backtest(
+    YahooDataBacktesting, start_date, end_date, parameters={"symbol": "SPY"}
+)
