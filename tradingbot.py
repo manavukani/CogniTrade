9b4bc15b9d13bd792d4289010b9453d744aa8acc
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2024-07-13 15:44:01 +0530

    added news fetching

--- tradingbot.py
+++ tradingbot.py
@@ -19,22 +19,38 @@ ALPACA_CREDS = {"API_KEY": API_KEY, "API_SECRET": API_SECRET, "PAPER": True}
 
 
 class MLTrader(Strategy):
-    def initialize(self, symbol: str = "SPY", cash_at_risk:float = 0.5):
+    def initialize(self, symbol: str = "SPY", cash_at_risk: float = 0.5):
         self.symbol = symbol
         self.sleeptime = "24H"
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
+        self.api = REST(key_id=API_KEY, secret_key=API_SECRET, base_url=BASE_URL)
 
-    def position_sizing(self): 
-        cash = self.get_cash() 
+    def position_sizing(self):
+        cash = self.get_cash()
         last_price = self.get_last_price(self.symbol)
         quantity = round(cash * self.cash_at_risk / last_price, 0)
         return cash, last_price, quantity
 
+    def get_dates(self):
+        end_date = self.get_datetime()
+        start_date = end_date - timedelta(days=3)
+        return end_date.strftime("%Y-%m-%d"), start_date.strftime("%Y-%m-%d")
+
+    def get_news(self):
+        end_date, start_date = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol, start=start_date, end=end_date)
+        news = [event.__dict__["_raw"]["headline"] for event in news]
+        return news        
+
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing()
-        
+
         if cash > last_price:
+            # testing news
+            news = self.get_news()
+            print(news)
+            
             if self.last_trade == None:
                 order = self.create_order(
                     self.symbol,
@@ -52,8 +68,13 @@ start_date = datetime(2024, 6, 1)
 end_date = datetime(2024, 6, 30)
 
 broker = Alpaca(ALPACA_CREDS)
-strategy = MLTrader(name="mlstrat", broker=broker, parameters={"symbol": "SPY", "cash_at_risk": 0.5})
+strategy = MLTrader(
+    name="mlstrat", broker=broker, parameters={"symbol": "SPY", "cash_at_risk": 0.5}
+)
 
 strategy.backtest(
-    YahooDataBacktesting, start_date, end_date, parameters={"symbol": "SPY", "cash_at_risk": 0.5}
+    YahooDataBacktesting,
+    start_date,
+    end_date,
+    parameters={"symbol": "SPY", "cash_at_risk": 0.5},
 )
